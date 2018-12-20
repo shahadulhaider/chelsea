1e80c04bcee3d09dfd9df78513ed4a4a9a0d0e58
Shahadul Haider <shahadul.haider@gmail.com>
2018-12-20 17:11:08 +0600

    featured section completed

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Layout from './Hoc/Layout'
+import Home from './Components/home'
 
 const routes = (props) => {
   return (
     <Layout>
-      second line
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </Layout>
   )
 }
