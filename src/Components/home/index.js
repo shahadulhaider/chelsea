834db128057d2f0950676ceb2747cb79c6a7c11d
Shahadul Haider <shahadul.haider@gmail.com>
2018-12-20 21:24:08 +0600

    style updated

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import Featured from './featured'
+import Matches from './matches'
 
 const Home = () => {
   return (
     <div className="bck_lblue">
       <Featured />
+      <Matches />
     </div>
   )
 }
---
+++ src/Components/home/matches/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MatchesHome = () => {
+  return (
+    <div className="home_matches_wrapper">
+      <div className="container">
+        h1
+      </div>
+    </div>
+  )
+}
+
+export default MatchesHome
