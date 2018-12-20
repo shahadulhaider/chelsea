866a5cafca7b916cde1abb204def89fa553c48ef
Shahadul Haider <shahadul.haider@gmail.com>
2018-12-20 23:54:58 +0600

    match sextion partially completed

--- src/Components/home/matches/index.js
+++ src/Components/home/matches/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
+import { Tag } from '../../ui/misc'
+
 const MatchesHome = () => {
   return (
     <div className="home_matches_wrapper">
       <div className="container">
-        h1
+        <Tag
+          bck="#003366"
+          size="58px"
+          color="#ffffff"
+        >
+          Matches
+        </Tag>
+
+        Block
+
+        <Tag
+          bck="#ffffff"
+          size="22px"
+          color="#003366"
+          link={true}
+          linkTo="/matches"
+        >
+          See more matches
+        </Tag>
       </div>
     </div>
   )
